d74f2f80b03f7f5aacd0d0230ebf2000df3c71be
herman <hermanconnor11@gmail.com>
2025-07-02 19:07:21 -0400

    edit tsconfig, create env file, create database schema

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,16 @@
-import { serve } from '@hono/node-server'
-import { Hono } from 'hono'
-
-const app = new Hono()
+import { serve } from '@hono/node-server';
+import app from './app.js';
 
 app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+  return c.text('Hello Hono!');
+});
 
-serve({
-  fetch: app.fetch,
-  port: 3000
-}, (info) => {
-  console.log(`Server is running on http://localhost:${info.port}`)
-})
+serve(
+  {
+    fetch: app.fetch,
+    port: 3000,
+  },
+  (info) => {
+    console.log(`Server is running on http://localhost:${info.port}`);
+  },
+);
